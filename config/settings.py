b76f1b3fc36ee98fe975a7c9e614c9771150de23
Catarina Aguiar <catarina.aguiar3@gmail.com>
2025-11-11 14:00:26 -0300

    :bug: Fix: Atualizar  o arquivo settings.py para corrigir a configuração do AWS S3

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,10 +141,11 @@ AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400", # Permitir cache por 1 dia
+    "CacheControl": "max-age=86400",
 }
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
+
 
 # Configuração de arquivos estáticos e mídia
 STORAGES = {
@@ -159,12 +157,14 @@ STORAGES = {
     },
 }
 
+
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 
 # URLs para arquivos de mídia
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
 
 # # Configurações locais para desenvolvimento
 # if DEBUG:
