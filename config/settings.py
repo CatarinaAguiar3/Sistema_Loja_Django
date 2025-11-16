6a02be6ced65b2dbb56c6634cad6f221af27113a
Catarina Aguiar <catarina.aguiar3@gmail.com>
2025-11-15 21:10:46 -0300

    :bug: Fix: Atualizar a chave secreta no settings.py para um valor fixo

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-1vf!$i5)(5t+%+435iqo$nr31)gv1lm--gxm#ys$1#4ca_4bv$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
